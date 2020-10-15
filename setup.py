56456de756d9a9197826aa9db71096aefa60799e
Angus Gibson <angus@agibson.me>
2020-10-15 13:12:27 +1100

    Add numpy include dirs to setup

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import Extension, setup
+import numpy
 
 setup(
     ext_modules=[Extension("sosfilt._sosfilt", ["sosfilt/_sosfilt.pyx"])],
+    include_dirs=[numpy.get_include()],
 )
