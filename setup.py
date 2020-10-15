d0371da7345b68b406104c8b94b26789f0f72593
Angus Gibson <angus@agibson.me>
2020-10-15 12:11:36 +1100

    Fix Extension definition in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import Extension, setup
 
 setup(
-    ext_modules=[Extension("sosfilt/_sosfilt", ["sosfilt/_sosfilt.pyx"])],
+    ext_modules=[Extension("sosfilt._sosfilt", ["sosfilt/_sosfilt.pyx"])],
 )
