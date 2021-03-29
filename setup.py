7faffa731eee9c60e660faa3ef85fc5188f256ad
Angus Gibson <angus@agibson.me>
2021-03-29 16:52:03 +1100

    Build Pythran extension for distribution
    This brings Pythran in as a dependency, and uses it through setuptools
    to automatically generate and build the required extension.
    
    As a note, however, the Pythran-ised code should be entirely backwards
    compatible with regular Python should Pythran not be available,
    although the speed benefits will not be present in this mode. The
    distribution won't perform this fallback automatically, either.

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import Extension, setup
+from pythran.dist import PythranExtension
 import numpy
 
 setup(
-    ext_modules=[Extension("sosfilt._sosfilt", ["sosfilt/_sosfilt.pyx"])],
+    ext_modules=[
+        Extension("sosfilt._sosfilt", ["sosfilt/_sosfilt.pyx"]),
+        PythranExtension("sosfilt._zpk_funcs", ["sosfilt/_zpk_funcs.py"]),
+    ],
     include_dirs=[numpy.get_include()],
 )
